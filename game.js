ae05aba827c2db15dd94bff2dbaf4608581db314
Kye Bracey <kye.bracey@gmail.com>
2019-04-15 00:06:46 +0100

    MVP - adds a cookies display and a big cookie which can be clicked to earn 1 cookie

--- game.js
+++ game.js
@@ -1,4 +1,20 @@
-// Nothing to see here...yet
-// Coming soon: 13,000 lines of spaghetti
+class Game {
+  init() {
+    this.bigCookie = document.getElementById('bigCookie');
+    this.cookiesDisplay = document.getElementById('cookiesDisplay'); // Shows the current number of cookies
+    this.currentCookies = 0;
+    this.cookiesPerClick = 1;
 
-console.log('test');
+    this.cookiesDisplay.innerHTML = `${this.currentCookies} cookies`;
+    this.bigCookie.addEventListener('click', this.clickCookie.bind(this));
+  }
+
+  clickCookie() {
+    this.addCookies(this.cookiesPerClick);
+  }
+
+  addCookies(amount) {
+    this.currentCookies += amount;
+    this.cookiesDisplay.innerHTML = `${this.currentCookies} cookies`;
+  }
+}
